c7d1e6865ac9a89652b03e000aa5ce4f8cf585eb
Mark <Mark@SurfacePro>
2018-10-23 14:40:53 +0100

    Refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserList from './UserList';
 
 export default class App extends React.Component {
 
@@ -23,34 +24,7 @@ export default class App extends React.Component {
     render() {
 
         return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Username</th>
-                        <th>Job</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.users.map((user, index) => {
-                        return (
-                            <tr key={user.id}>
-                                <td>#{user.id}</td>
-                                <td>{user.username}</td>
-                                <td>{user.job}</td>
-                                <td>
-                                    <a href={'/user-edit/' + user.id}>Edit</a>
-                                </td>
-                                <td>
-                                    <button data-id={user.id}>Delete</button>
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+            <UserList users={this.state.users}/>
         );
     }
 }
